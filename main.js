ae53057f8634e296a494927dda15cc138fe2786d
Sayz Lim <github@sayzlim.net>
2022-07-05 23:16:42 +0700

    build: site update

--- main.js
+++ main.js
@@ -37,3 +37,41 @@ document.addEventListener('scroll', function (e) {
 		ticking = true;
 	}
 });
+
+// Page Visibility API
+let hidden;
+let visibilityChange;
+if (typeof document.hidden !== 'undefined') {
+	// Opera 12.10 and Firefox 18 and later support
+	hidden = 'hidden';
+	visibilityChange = 'visibilitychange';
+} else if (typeof document.msHidden !== 'undefined') {
+	hidden = 'msHidden';
+	visibilityChange = 'msvisibilitychange';
+} else if (typeof document.webkitHidden !== 'undefined') {
+	hidden = 'webkitHidden';
+	visibilityChange = 'webkitvisibilitychange';
+}
+
+function handleVisibilityChange() {
+	if (!document[hidden]) {
+		if (typeof _carbonads !== 'undefined')
+			_carbonads.refresh();
+	}
+}
+
+// Warn if the browser doesn't support addEventListener or the Page Visibility API
+if (
+	typeof document.addEventListener === 'undefined' ||
+	hidden === undefined
+) {
+	console.log(
+		'This demo requires a browser, such as Google Chrome or Firefox, that supports the Page Visibility API.'
+	);
+} else {
+	document.addEventListener(
+		visibilityChange,
+		handleVisibilityChange,
+		false
+	);
+}
