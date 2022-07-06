c6f089d03aea5c0534ac02607507f60036bb11de
Sayz Lim <github@sayzlim.net>
2022-07-06 11:03:02 +0700

    build: site update

--- main.js
+++ main.js
@@ -39,39 +39,50 @@ document.addEventListener('scroll', function (e) {
 });
 
 // Page Visibility API
-let hidden;
-let visibilityChange;
-if (typeof document.hidden !== 'undefined') {
-	// Opera 12.10 and Firefox 18 and later support
-	hidden = 'hidden';
-	visibilityChange = 'visibilitychange';
-} else if (typeof document.msHidden !== 'undefined') {
-	hidden = 'msHidden';
-	visibilityChange = 'msvisibilitychange';
-} else if (typeof document.webkitHidden !== 'undefined') {
-	hidden = 'webkitHidden';
-	visibilityChange = 'webkitvisibilitychange';
-}
+const _carbonOptimize = {
+  isRefreshAble: function () {
+    return !(
+      typeof document.addEventListener === 'undefined' ||
+      this.browserSupport().hidden === undefined
+    );
+  },
+  browserSupport: function () {
+    let hidden;
+    let visibilityChange;
+    if (typeof document.hidden !== 'undefined') {
+      // Opera 12.10 and Firefox 18 and later support
+      hidden = 'hidden';
+      visibilityChange = 'visibilitychange';
+    } else if (typeof document.msHidden !== 'undefined') {
+      hidden = 'msHidden';
+      visibilityChange = 'msvisibilitychange';
+    } else if (
+      typeof document.webkitHidden !== 'undefined'
+    ) {
+      hidden = 'webkitHidden';
+      visibilityChange = 'webkitvisibilitychange';
+    }
+    return {
+      hidden: hidden,
+      visibilityChange: visibilityChange,
+    };
+  },
+  handleVisibilityChange: function () {
+    if (!document.hidden) {
+      console.log('State triggered');
+      if (typeof _carbonads !== 'undefined')
+        _carbonads.refresh();
+    }
+  },
+  init: function () {
+    if (this.isRefreshAble()) {
+      document.addEventListener(
+        this.browserSupport().visibilityChange,
+        this.handleVisibilityChange,
+        false
+      );
+    }
+  },
+};
 
-function handleVisibilityChange() {
-	if (!document[hidden]) {
-		if (typeof _carbonads !== 'undefined')
-			_carbonads.refresh();
-	}
-}
-
-// Warn if the browser doesn't support addEventListener or the Page Visibility API
-if (
-	typeof document.addEventListener === 'undefined' ||
-	hidden === undefined
-) {
-	console.log(
-		'This demo requires a browser, such as Google Chrome or Firefox, that supports the Page Visibility API.'
-	);
-} else {
-	document.addEventListener(
-		visibilityChange,
-		handleVisibilityChange,
-		false
-	);
-}
+_carbonOptimize.init();
